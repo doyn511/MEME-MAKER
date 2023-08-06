c0cb083f210658858f513e2836cdc083a0a1b33c
Doyun Lee <doyn511@ajou.ac.kr>
2023-08-06 14:27:37 +0900

    [feat] Change Stroke Color

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const lineWidth = document.getElementById("line-width");
+const color = document.getElementById("color");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d"); // 캔버스에 그림을 그릴 때 사용하는 브러쉬
 canvas.width = 800;
@@ -28,9 +29,15 @@ function onLineWidthChange(event){
   ctx.lineWidth = event.target.value;
 }
 
+function onColorChange(event){
+  ctx.strokeStyle = event.target.value;
+  ctx.fillStyle = event.target.value;
+}
+
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
 
-lineWidth.addEventListener("change", onLineWidthChange)
+lineWidth.addEventListener("change", onLineWidthChange);
+color.addEventListener("change", onColorChange);
