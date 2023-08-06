53c003eea03240639671eb58c2f27ee19d0bc2c1
Doyun Lee <doyn511@ajou.ac.kr>
2023-08-06 13:20:13 +0900

    [feat] Change Line Width

--- app.js
+++ app.js
@@ -1,26 +1,9 @@
+const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d"); // 캔버스에 그림을 그릴 때 사용하는 브러쉬
 canvas.width = 800;
 canvas.height = 800;
-const colors = [
-  "#ff3838",
-  "#ffb8b8",
-  "#c56cf0",
-  "#ff9f1a",
-  "#ff9f1a",
-  "#32ff7e",
-  "#7efff5",
-  "#18dcff",
-  "#7d5fff",
-]
-
-// ctx.beginPath();
-// ctx.fill();
-// ctx.stroke();
-// ctx.moveTo();
-// ctx.lineTo();
-
-ctx.lineWidth = 2;
+ctx.lineWidth = lineWidth.value;
 let isPainting = false;
 
 function onMove(event){
@@ -37,10 +20,17 @@ function startPainting(){
 }
 
 function cancelPainting(){
+  ctx.beginPath();
   isPainting = false;
+}
+
+function onLineWidthChange(event){
+  ctx.lineWidth = event.target.value;
 }
 
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
+
+lineWidth.addEventListener("change", onLineWidthChange)
